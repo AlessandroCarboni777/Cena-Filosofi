d45c8eb1f537ba99dbb0a2b4025ad803a5bb9eb5
Alessandro Carboni <93706866+AlessandroCarboni777@users.noreply.github.com>
2023-07-03 14:55:08 +0200

    Carboni.sh

---
+++ Carboni.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gcc -o filosofi Carboni.c
+echo "INSERIRE IL NUMERO DI FILOSOFI:   "
+read filosofi
+echo "INSERIRE IL FLAG DELLO STALLO (negativo/nullo --> disattivato, altrimenti attivato):   "
+read stallo
+echo "INSERIRE IL FLAG DI SOLUZIONE ALLO STALLO (negativo/nullo --> disattivato, altrimenti attivato):   "
+read soluzione
+echo "INSERIRE IL FLAG DELLA STARVATION (negativo/nullo --> disattivato, altrimenti attivato):   "
+read starvation
+sleep 5
+clear
+echo "ESECUZIONE IN CORSO..."
+./filosofi $filosofi $stallo $soluzione $starvation
